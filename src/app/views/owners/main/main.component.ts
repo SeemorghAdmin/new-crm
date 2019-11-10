4557a104377ad63ea12bf82a27e661b474c5abd0
SeemorghAdmin <Seemorgh-130@hotmail.com>
2019-11-10 10:57:44 +0330

    fixed routing bugs again

--- src/app/views/owners/main/main.component.ts
+++ src/app/views/owners/main/main.component.ts
@@ -17,8 +17,8 @@ export class OwnersMainComponent implements OnInit, OnDestroy {
   ////////////////// این خط ها متغیرهای مورد نیاز برای sidenav را ایجاد میکنند
   userType = "Owner" // نام صفحه
   fillerNav: MenuItems[] = [
-    {name: "اضافه کردن کاربر جدید", url:"/owner/home/AddDeveloperComponent"},
-    {name: "درخواست های پشتیبانی", url:"/owner/home/ShowTicketsComponent"},
+    {name: "اضافه کردن کاربر جدید", url:"/owner/home/AddPerson"},
+    {name: "درخواست های پشتیبانی", url:"/owner/home/ShowTickets"},
   ]; // ایجاد 50 تا آیتم برای نمایش در منو
    mobileQuery: MediaQueryList;
   private _mobileQueryListener: () => void;
