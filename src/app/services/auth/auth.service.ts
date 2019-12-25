e90e789075f1f129f2448bba5cc11b93d8312377
SeemorghAdmin <Seemorgh-130@hotmail.com>
2019-12-25 14:14:15 +0330

    fixed console log

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -11,12 +11,12 @@ export class AuthService {
   GetInfoById(strID) {
 
 
-    ///http://crm.nren.ir  
-    this.http.get('http://localhost:8080/nern_test_war_exploded//api/get-login-info.jsp?id=' + strID).subscribe(
-      res => {
-        console.log(res);
-      }
-    );
+    ///http://crm.nren.ir
+    // this.http.get('http://localhost:8080/nern_test_war_exploded//api/get-login-info.jsp?id=' + strID).subscribe(
+    //   res => {
+    //     console.log(res);
+    //   }
+    // );
   }
 
 }
