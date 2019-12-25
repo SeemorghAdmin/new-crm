7cce2e4a069002d44c9f4c8d9e24078ad4ca7a5e
saeed173 <saeed.taghizade173@gmail.com>
2019-12-25 15:19:36 +0330

    Edit ServiceFileUpload

--- src/app/views/owners/uni-service-requst/service-file-upload/service-file-upload.component.ts
+++ src/app/views/owners/uni-service-requst/service-file-upload/service-file-upload.component.ts
@@ -36,7 +36,7 @@ export class ServiceFileUploadComponent implements OnInit {
     const formData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
     formData.append('type', 'final-signed-form');
-    // formData.append('uNumber',this.uNumber);
+    formData.append('uNumber',this.uNumber);
     this.response = formData;
     this.ta = fileToUpload.name;
   }
@@ -48,7 +48,7 @@ export class ServiceFileUploadComponent implements OnInit {
     const formData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
     formData.append('type', 'letter');
-    // formData.append('uNumber',this.uNumber);
+    formData.append('uNumber',this.uNumber);
     this.response1 = formData;
     this.tb = fileToUpload.name;
   }
@@ -60,19 +60,21 @@ export class ServiceFileUploadComponent implements OnInit {
     const formData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
     formData.append('type', 'post-reciept');
-    // formData.append('uNumber',this.uNumber);
+    formData.append('uNumber',this.uNumber);
     this.response2 = formData;
     this.tc = fileToUpload.name;
   }
   upload() {
     this.http.post(this.BaseURI + '/ServiceFileUpload2', this.response, { reportProgress: true, observe: 'events' })
       .subscribe(event2 => {
+        console.log(event2);
         if (event2.type === HttpEventType.UploadProgress)
           this.progress = Math.round(100 * event2.loaded / event2.total);
         else if (event2.type === HttpEventType.Response) {
           this.message = 'آپلود انجام شد';
           this.http.post(this.BaseURI + '/ServiceFileUpload1', this.response1, { reportProgress: true, observe: 'events' })
             .subscribe(event2 => {
+              console.log(event2);
               if (event2.type === HttpEventType.UploadProgress)
                 this.progress1 = Math.round(100 * event2.loaded / event2.total);
               else if (event2.type === HttpEventType.Response) {
